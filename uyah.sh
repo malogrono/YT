2c0fc83521e6dbc2b71d3e4057590ea945b969e2
malogrono <136266511+malogrono@users.noreply.github.com>
2024-11-16 22:17:56 +0700

    Update uyah.sh

--- uyah.sh
+++ uyah.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.WUXIA
+./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RJAkiJXQy8Q9PcBkEPTBypMJj7ofGgQjo6.woy
 while [ 1 ]; do
 sleep 3
 done
